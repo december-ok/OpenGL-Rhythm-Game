7147f5632e4beebd19cdc7a3225e1abeedc70058
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2022-11-17 17:01:40 +0900

    fix: virtualize Note func // add: SectionNote func
    Note 클래스 함수들을 virtualize하였습니다. 또한 롱노트 구현과 관련된 함수들을 추가하였습니다.

--- OpenGL Project/Note.cpp
+++ OpenGL Project/Note.cpp
@@ -2,7 +2,7 @@
 #include "RoundScene.h"
 #include <iostream>
 
-float Note::GetHeight(unsigned int _frame)
+int Note::GetHeight(unsigned int _frame)
 {
 	if (!IsActive(_frame)) return ROWS; // Non-active
 	return ROWS - (_frame - this->createFrame); // 
@@ -14,20 +14,21 @@ bool Note::IsCheckCreate(unsigned int _frame)
 
 }
 
-bool Note::IsActive(unsigned int _frame)
-{
-	//cout << _frame << " | " << this->createFrame << endl;
-	return (this->createFrame < _frame&& this->createFrame + ROWS >= _frame);
-}
+//bool Note::IsActive(unsigned int _frame)
+//{
+//	//cout << _frame << " | " << this->createFrame << endl;
+//	return (this->createFrame < _frame&& this->createFrame + ROWS >= _frame);
+//}
 
 /*우선은 이렇게만 기능 구현. 세부 기능은 조정해야 함*/
-bool Note::IsMissNote(unsigned int _frame) {
-	return this->createFrame + ROWS < _frame && this->isAlive;
-}
+//bool Note::IsMissNote(unsigned int _frame) {
+//	return this->createFrame + ROWS < _frame && this->isAlive;
+//}
 
 void Note::killNote() {
 	this->isAlive = false;
 }
+
 
 void ItemNote::UseItem()
 {
--- OpenGL Project/Note.h
+++ OpenGL Project/Note.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "Global.h"
+#include <iostream>
 class Note
 {
 public:
@@ -7,11 +9,13 @@ public:
 	unsigned char type; // 노트의 종류 (0 = Normal, 1 = Section, 2 = Lie, 3 = Item)
 
 	~Note() {};
-	virtual float GetHeight(unsigned int _frame); // 현재 fps에서 위치 값을 반환하는 함수 (만약 Active된 상태가 아니라면 0을 반환)
+	int GetHeight(unsigned int _frame); // 현재 fps에서 위치 값을 반환하는 함수 (만약 Active된 상태가 아니라면 0을 반환)
 	bool IsCheckCreate(unsigned int _frame); // 생성 되어야 하는지 체크하고 반환하는 함수
-	bool IsActive(unsigned int _frame); // 현재 월드에 Active한 상태인지 체크하고 반환하는 함
-	bool IsMissNote(unsigned int _frame);	// 사용자가 입력하지 못한 노트인지 확인
 	void killNote();	// 사용자의 입력이 들어오면 노트를 죽임
+
+	virtual bool IsActive(unsigned int _frame) = 0; // 현재 월드에 Active한 상태인지 체크하고 반환하는 함
+	virtual bool IsMissNote(unsigned int _frame) = 0;	// 사용자가 입력하지 못한 노트인지 확인
+	virtual int getNoteLength() = 0;	// 노트의 길이(일반 노트: 1, 롱노트: n)
 };
 
 class NoramlNote : public Note
@@ -19,30 +23,70 @@ class NoramlNote : public Note
 public:
 	NoramlNote(int _createTime) { this->type = 0; this->createFrame = _createTime; };
 	~NoramlNote() {};
+	int getNoteLength() override { return 1; }
+	bool IsActive(unsigned int _frame) override { return (this->createFrame < _frame&& this->createFrame + ROWS >= _frame); }
+	bool IsMissNote(unsigned int _frame) override { return this->createFrame + ROWS < _frame&& this->isAlive; }
+
 };
 
 class SectionNote : public Note
 {
-public:
 	int sectionTime; // 지속되는 시간 (시작점은 createTime을 기준)
+	int endFrame;	// 종료되야 하는 frame
+public:
 
-	SectionNote(int _createTime, int _sectionTime) { this->type = 1; this->createFrame = _createTime; this->sectionTime = _sectionTime; };
+	SectionNote(int _createTime, int _endFrame) { this->type = 1; this->createFrame = _createTime; this->endFrame = _endFrame; this->sectionTime = this->endFrame - this->createFrame; };
 	~SectionNote() {};
+	int getNoteLength() override { return sectionTime; }
+	bool IsActive(unsigned int _frame) override { return (this->createFrame < _frame&& this->endFrame + ROWS >= _frame); }
+	bool IsMissNote(unsigned int _frame) override { return (this->endFrame + ROWS < _frame&& this->isAlive); }
+	int getEndFrame() { return endFrame; }
+
+
 };
 
+// 가짜 노트
 class LieNote : public Note
 {
 public:
 	LieNote(int _createTime) { this->type = 2; this->createFrame = _createTime; };
 	~LieNote() {};
+	int getNoteLength() override { return 1; }
+	bool IsActive(unsigned int _frame) override { return (this->createFrame < _frame&& this->createFrame + ROWS >= _frame); }
+	bool IsMissNote(unsigned int _frame) override { return this->createFrame + ROWS < _frame&& this->isAlive; }
+
+
 };
 
+// 가짜 롱노트
+class LieSectionNote : public Note
+{
+	int sectionTime; // 지속되는 시간 (시작점은 createTime을 기준)
+	int endFrame;	// 종료되야 하는 frame
+
+public:
+	LieSectionNote(int _createTime, int _endFrame) { this->type = 3; this->createFrame = _createTime; this->endFrame = _endFrame; this->sectionTime = this->endFrame - this->createFrame;
+	};
+	~LieSectionNote() {};
+	int getNoteLength() override { return sectionTime; }
+	bool IsActive(unsigned int _frame) override { return (this->createFrame < _frame&& this->endFrame + ROWS >= _frame); }
+	bool IsMissNote(unsigned int _frame) override { return this->endFrame + ROWS < _frame&& this->isAlive; }
+
+};
+
+// 아이템 노트(4)
 class ItemNote : public Note
 {
 public:
+	
 	unsigned char itemType;
 
-	ItemNote(int _createTime, unsigned char _itemType) { this->type = 3; this->createFrame = _createTime; this->itemType = _itemType; };
+	ItemNote(int _createTime, unsigned char _itemType) { this->type = 4; this->createFrame = _createTime; this->itemType = _itemType; };
 	~ItemNote() {};
 	void UseItem();
+	int getNoteLength() override { return 1; }
+	bool IsActive(unsigned int _frame) override { return (this->createFrame < _frame&& this->createFrame + ROWS >= _frame); }
+	bool IsMissNote(unsigned int _frame) override { return this->createFrame + ROWS < _frame&& this->isAlive; }
+
+
 };
